6a48cbba6f20e4e695e677a11bd5b798d475231c
Paul Bastowski <pbastowski@gmail.com>
2015-06-03 23:50:36 +0200

    Made the code teh same as mrt:exports and added a very basic require function. Bumped version to 0.2.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'pbastowski:require',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Simple require stub',
   // URL to the Git repository containing the source code for this package.
--- require.js
+++ require.js
@@ -1,30 +1,37 @@
-(function (global) {                                                          // 2
+// Pass the global object by reference
+(function (global) {
     require = function (module) {
         return exports[module];
     }
-    // Initialize an empty module object to be referenced                       // 3
-    module = {};                                                                // 4
-                                                                                // 5
-    // Set getters and setters for `module.exports` and the `exports` alias     // 6
-    var name = 'exports';                                                       // 7
-    if (typeof Object.getOwnPropertyDescriptor(module, name) === 'undefined') { // 8
-        // Extend the global namespace from the local object                      // 9
-        Object.defineProperty(module, name, {                                     // 10
-            // Set a setter to handle the object's properties                       // 11
-            set: function (props) {                                                 // 12
-                // Export each local variable to the `this` object.                   // 13
-                for (var prop in props) {                                             // 14
-                    // Make sure we aren't inheriting any prototype properties          // 15
-                    if (props.hasOwnProperty(prop)) {                                   // 16
-                        global[prop] = props[prop];                                       // 17
-                    }                                                                   // 18
-                }                                                                     // 19
-            },                                                                      // 20
-            get: function () {                                                      // 21
-                return global;                                                        // 22
-            }                                                                       // 23
-        });                                                                       // 24
-    }                                                                           // 25
-    // Set `exports` to the original value of `module.exports`                  // 26
-    exports = module.exports;                                                   // 27
-}(this));
+
+    // Initialize an empty module object to be referenced
+    module = {};
+
+    // Set getters and setters for `module.exports` and the `exports` alias
+    var name = 'exports';
+    if (typeof Object.getOwnPropertyDescriptor(module, name) === 'undefined') {
+        // Extend the global namespace from the local object
+        Object.defineProperty(module, name, {
+            // Set a setter to handle the object's properties
+            set: function (props) {
+                // Export each local variable to the `this` object.
+                for (var prop in props) {
+                    // Make sure we aren't inheriting any prototype properties
+                    if (props.hasOwnProperty(prop)) {
+                        global[prop] = props[prop];
+                    }
+                }
+            },
+            get: function () {
+                return global;
+            }
+        });
+    }
+    // Set `exports` to the original value of `module.exports`
+    exports = module.exports;
+})((function () {
+    // Pass top-level (global) object to the above function
+    return (function () {
+        return this;
+    }).call(null); // Force `this` to be the top-level object.
+})());
